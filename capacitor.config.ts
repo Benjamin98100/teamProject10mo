36253a14d3c90f89b9d35e1417a2840d82ed30da
benjasx <benjasanchez175@gmail.com>
2022-10-24 21:48:21 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'breaking_bad',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
